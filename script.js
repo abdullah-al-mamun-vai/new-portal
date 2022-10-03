f3258304e5bd6969c2ca48247c65e47b14f73246
abdullah-al-mamun-vai <mdm66304@gmail.com>
2022-10-03 23:46:58 +0600

    category added

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-// const clickhere = () => {
-//     fetch(``)
-//         .then(res => res.json())
-//         .then(data => console.log(data))
-// }
+const clickhere = () => {
+    fetch(`https://openapi.programming-hero.com/api/news/categories`)
+        .then(res => res.json())
+        .then(data => NewsCategories(data.data.news_category))
+        .catch(error => console.log(error))
+}
+const NewsCategories = (categories) => {
+    const getCategory = document.getElementById('categories');
+    categories.forEach(element => {
+        const li = document.createElement('li')
+        li.innerHTML = `
+        <a>${element.category_name}</a>
+        `
+        getCategory.appendChild(li)
+    });
+}
+clickhere()
