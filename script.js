cf4832d4528c71679f1c7f5a6de6fdeb11e23d3a
abdullah-al-mamun-vai <mdm66304@gmail.com>
2022-10-05 01:02:35 +0600

    modal add

--- script.js
+++ script.js
@@ -31,36 +31,67 @@ const loadNews = (newses) => {
         <figure><img class="h-64 w-52" src="${thumbnail_url}" alt="Movie" /></figure>
         <div class="card-body">
             <h2 class="card-title">${title}</h2>
-            <p>${details?.slice(0, 100)}</p>
+            <p class="card-p-style">${details ? details.slice(0, 100) : "no text found"}</p>
 
-            <div class="card-actions justify-between">
-                <div>
-                    <img src="" alt="">
+            <div class="card-actions justify-between items-center">
+                <div class="flex">
+                    <img class="w-9 mr-3" src="${author.img}" alt="">
                     <div>
                         <p>${author.name ? author.name : "Not Found"}</p>
                         <p>${author.published_date ? author.published_date : "not found"} </p>
                     </div>
                 </div>
                 <div>
-                    <p>${total_view}</p>
+                    <p><i class="fa-solid fa-eye"></i>${total_view}</p>
                 </div>
-                <label for="my-modal-4" class="btn ">Watch</button>
+                <label for="my-modal-4" class="btn modal-button" onclick="getModal('${news._id}')">Watch</label>
             </div>
 
-            <!-- The button to open modal -->
+           
 
-<!-- Put this part before </body> tag -->
-<input type="checkbox" id="my-modal-4" class="modal-toggle" />
-<label for="my-modal-4" class="modal cursor-pointer">
-  <label class="modal-box relative" for="">
-    <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
-    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-  </label>
-</label>
+          
         `
         getNews.appendChild(div);
-        console.log(news);
+        // console.log(news);
     })
 
 }
+
+const getModal = (id) => {
+    fetch(`https://openapi.programming-hero.com/api/news/${id}`)
+        .then(res => res.json())
+        .then(data => setModal(data.data))
+        .catch(error => console.log(error))
+}
+const setModal = modalData => {
+    const modalCon = document.getElementById('modal-contain')
+    modalCon.innerHTML = ""
+    modalData.forEach(single => {
+        console.log(single)
+        const { author, thumbnail_url, title, details, total_view } = single;
+        const div = document.createElement('div')
+        div.classList.add("modal-box", "relative")
+        div.innerHTML = `
+        <label class="" for="">
+                <img class="h-64 w-52" src="${thumbnail_url}" alt="Movie" />
+                <h2 class="card-title">${title}</h2>
+                <p >${details}</p>
+                <div class="flex justify-evenly items-center">
+                    <div class="flex">
+                        <img class="w-9 mr-3" src="${author.img}" alt="">
+                        <div>
+                            <p>${author.name ? author.name : "Not Found"}</p>
+                            <p>${author.published_date ? author.published_date : "not found"} </p>
+                        </div>
+                    </div>
+                    <div>
+                        <p><i class="fa-solid fa-eye"></i>${total_view}</p>
+                    </div>
+                </div>
+            </label>
+        `
+        modalCon.appendChild(div);
+    })
+}
 clickhere()
+clickCategory(08)
